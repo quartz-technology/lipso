8535452dfdd5d4de02573d7d3393f428b483e708
Luca Georges Francois <luca@quartz.technology>
2024-05-08 15:33:01 +0200

    feat(backend): add /positions DTO
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>

--- backend/src/positions/dto/list-positions.dto.ts
+++ backend/src/positions/dto/list-positions.dto.ts
@@ -1,1 +1,15 @@
-export class ListPositionsDTO {}
+import { IsNotEmpty } from 'class-validator';
+
+/**
+ * Parameters for the `/positions` endpoint, used to specify:
+ *
+ * - The Ethereum Address of the owner of the positions.
+ */
+export class ListPositionsDTO {
+  // TODO: Validate that the owner is a valid Ethereum Address.
+  @IsNotEmpty()
+  owner: string;
+
+  // TODO: Add a field to specify the list of chains.
+  // TODO: Add a field to specify the list of protocols.
+}
