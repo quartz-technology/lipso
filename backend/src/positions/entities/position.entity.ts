b8d2722e2ba9bfeaea5d1e0d87f04b75e65b610c
Luca Georges Francois <luca@quartz.technology>
2024-05-08 15:33:20 +0200

    feat(backend): add /positions RO
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>

--- backend/src/positions/entities/position.entity.ts
+++ backend/src/positions/entities/position.entity.ts
@@ -1,1 +1,11 @@
-export class Position {}
+/**
+ * A liquidity position on a DEX.
+ *
+ * As the goal of this project is to provide insights to the user about their liquidity positions, this entity helps to understand if their position is in range or not.
+ * Depending on the DEX, the extra field can be used to store additional information about the position.
+ * For example, in UniswapV3, the extra field stores the position ID can be used to create a link to the position on the official UniswapV3 interface.
+ */
+export class Position<T> {
+  inRange: boolean;
+  extra: T;
+}
