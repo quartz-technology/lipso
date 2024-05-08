a53ae3b3b97270477347236aa7a6074244c71091
Luca Georges Francois <luca@quartz.technology>
2024-05-08 16:21:07 +0200

    test(backend): fix broken UT
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>

--- backend/src/positions/positions.controller.spec.ts
+++ backend/src/positions/positions.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PositionsController } from './positions.controller';
 import { PositionsService } from './positions.service';
+import { ResolverRegistryService } from './resolver-registry/resolver-registry.service';
 
 describe('PositionsController', () => {
   let controller: PositionsController;
@@ -8,7 +9,7 @@ describe('PositionsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PositionsController],
-      providers: [PositionsService],
+      providers: [PositionsService, ResolverRegistryService],
     }).compile();
 
     controller = module.get<PositionsController>(PositionsController);
