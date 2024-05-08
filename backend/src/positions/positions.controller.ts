fe73931aef329389d78a0c67cd487d8bd801cb7d
Luca Georges Francois <luca@quartz.technology>
2024-05-08 16:17:21 +0200

    feat(backend): add new positions resource
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>

--- backend/src/positions/positions.controller.ts
+++ backend/src/positions/positions.controller.ts
@@ -2,13 +2,39 @@ import { Controller, Get, Query } from '@nestjs/common';
 import { PositionsService } from './positions.service';
 import { ListPositionsDTO } from './dto/list-positions.dto';
 import { Position } from './entities/position.entity';
+import { ResolverRegistryService } from './resolver-registry/resolver-registry.service';
+import { mainnet } from 'viem/chains';
+import { createPublicClient, getAddress, http } from 'viem';
+import { ResolverPositionExtra } from './resolver-registry/resolver.entity';
 
 @Controller('positions')
 export class PositionsController {
-  constructor(private readonly positionsService: PositionsService) {}
+  constructor(
+    private readonly positionsService: PositionsService,
+    private resolverRegistryService: ResolverRegistryService,
+  ) {
+    // TODO: Get HTTP endpoint from config.
+    const ethMainnetClient = createPublicClient({
+      chain: mainnet,
+      transport: http(undefined, {
+        batch: {
+          batchSize: 10,
+        },
+      }),
+    });
+
+    // TODO: Get UniswapV3 mainnet deployment addresses from config.
+    this.resolverRegistryService.registerUniswapV3Resolver(
+      ethMainnetClient,
+      getAddress('0x1F98431c8aD98523631AE4a59f267346ea31F984'),
+      getAddress('0xC36442b4a4522E871399CD717aBDD847Ab11FE88'),
+    );
+  }
 
   @Get()
-  async findAll(@Query() query: ListPositionsDTO): Promise<Position[]> {
-    return this.positionsService.findAll(query);
+  async findAll(
+    @Query() query: ListPositionsDTO,
+  ): Promise<Position<ResolverPositionExtra>[]> {
+    return this.positionsService.findAll(query, this.resolverRegistryService);
   }
 }
--- backend/src/positions/positions.service.ts
+++ backend/src/positions/positions.service.ts
@@ -1,12 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { ListPositionsDTO } from './dto/list-positions.dto';
 import { Position } from './entities/position.entity';
+import { ResolverRegistryService } from './resolver-registry/resolver-registry.service';
+import { getAddress } from 'viem';
+import { ResolverPositionExtra } from './resolver-registry/resolver.entity';
 
 @Injectable()
 export class PositionsService {
-  async findAll(query: ListPositionsDTO): Promise<Position[]> {
-    console.log('PositionsService.findAll()', query);
+  async findAll(
+    query: ListPositionsDTO,
+    resolverRegistryService: ResolverRegistryService,
+  ): Promise<Position<ResolverPositionExtra>[]> {
+    const owner = getAddress(query.owner);
+    const store = resolverRegistryService.getStore();
 
-    return [];
+    const positions = (
+      await Promise.all(
+        Array.from(store.values()).map((resolver) => {
+          return Promise.all(
+            resolver.map((r) =>
+              r.findAllPositions<ResolverPositionExtra>(owner),
+            ),
+          );
+        }),
+      )
+    )
+      .flat()
+      .flat();
+
+    return positions;
   }
 }
